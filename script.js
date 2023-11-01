b863bbf66bedff14e375f8bd0519668104119ee8
Gergo <104260836+Gergo0314@users.noreply.github.com>
2023-11-01 15:47:44 +0100

    create 4x4 grid amd add coloring function

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const grid = document.querySelector(".grid");
 
-function createDivs(num) {
+let createDivs = (num) => {
   for (let i = 0; i < num; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-
     grid.appendChild(cell);
+    
+    cell.addEventListener("mouseover", () => {
+      cell.style.backgroundColor = "black";
+    });
   }
-}
+};
 createDivs(16);
