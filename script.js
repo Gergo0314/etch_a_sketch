eb90e7bcb2779148bc81dab273c72da61f36113a
Gergo <104260836+Gergo0314@users.noreply.github.com>
2023-11-01 16:02:40 +0100

    add reset button with eventlistener

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const grid = document.querySelector(".grid");
+const reset = document.querySelector(".reset");
 
 let createDivs = (num) => {
   for (let i = 0; i < num; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
     grid.appendChild(cell);
-    
+
     cell.addEventListener("mouseover", () => {
       cell.style.backgroundColor = "black";
+    });
+
+    reset.addEventListener("click", () => {
+      cell.style.backgroundColor = "white";
     });
   }
 };
